faf6932ccb3504e090a08277030380d90869bf01
neyserfp <neyser@digitechfp.com>
2024-02-05 12:44:03 +0100

    Creación

---
+++ src/main/java/com/app/sockets/dao/ConversacionImpl.java
@@ -0,0 +1,44 @@
+package com.app.sockets.dao;
+
+import com.app.sockets.conexion.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConversacionImpl implements ConversacionDAO {
+
+    Conexion instanciaMySQL = Conexion.getInstance();
+
+    @Override
+    public void registrarConversacionA(String mensaje) {
+        PreparedStatement consulta = null;
+        Connection conexion = null;
+
+        try {
+            conexion = instanciaMySQL.conectar();
+            consulta = conexion.prepareStatement("insert into conversaciones(conversacionA) values(?)");
+
+            consulta.setString(1, mensaje);
+            consulta.executeUpdate();
+        } catch (SQLException error) {
+            System.out.println(error);
+        }
+    }
+
+    @Override
+    public void registrarConversacionB(String mensaje) {
+        PreparedStatement consulta = null;
+        Connection conexion = null;
+
+        try {
+            conexion = instanciaMySQL.conectar();
+            consulta = conexion.prepareStatement("insert into conversaciones(conversacionB) values(?)");
+
+            consulta.setString(1, mensaje);
+            consulta.executeUpdate();
+        } catch (SQLException error) {
+            System.out.println(error);
+        }
+    }
+
+}
